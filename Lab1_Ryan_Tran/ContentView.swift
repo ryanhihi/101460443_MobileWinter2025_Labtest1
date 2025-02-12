c268840e9c730d691b2afa1ef959eb57ab6e8259
Ryan Tran <101460443@clc330-07m.local>
2025-02-12 17:37:53 -0500

    Add validation icon

--- Lab1_Ryan_Tran/ContentView.swift
+++ Lab1_Ryan_Tran/ContentView.swift
@@ -25,15 +25,23 @@ struct ContentView: View {
                 .ignoresSafeArea()
             
             VStack (spacing: 20){
-                
+                //Tilte for explaination of the game
                 Text("Select if the number is \"Prime\" or \"Not Prime\"")
                     .font(.title)
                 Spacer()
                 
+                //Number displayed on the screen
                 Text("\(randomNumber)")
                     .font(.system(size: 60))
                     .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
                 
+                //Validation icon displayed on the screen when the answer selected or time passed
+                
+                if let validateIcon = validateIcon {
+                    Text(validateIcon)
+                        .font(.system(size: 50))
+                }
+                Spacer()
             }
             .padding()
         }
