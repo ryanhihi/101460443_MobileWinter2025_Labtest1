22c8041cf8186283dd775122e54335fa4bedcef9
Ryan Tran <101460443@clc330-07m.local>
2025-02-12 17:22:39 -0500

    Initial commit for project creation

---
+++ Lab1_Ryan_Tran/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lab1_Ryan_Tran
+//
+//  Created by Ryan Tran on 2025-02-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lab1_Ryan_Tran/Lab1_Ryan_TranApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1_Ryan_TranApp.swift
+//  Lab1_Ryan_Tran
+//
+//  Created by Ryan Tran on 2025-02-12.
+//
+
+import SwiftUI
+
+@main
+struct Lab1_Ryan_TranApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
